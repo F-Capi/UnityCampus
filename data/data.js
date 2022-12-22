75fd20c1f85260acd5df31dd280d966903ff63e3
Franco Capiglioni <fcapiglioni@uoc.edu>
2022-12-22 05:12:57 +0100

    a ver que tal

--- data/data.js
+++ data/data.js
@@ -4,42 +4,42 @@ export const data = [
         name: "prjt1",
         instructions: [{
             title: "Ventana - 1",
-            img: "./gifs/1.1Ventana.gif",
+            img: "../gifs/1.1Ventana.gif",
             text: "Scratch es como una obra de teatro hay:   <br><br>1.Un escenario donde estan los personajes.<br> 2. Un reparto de actores.<br> 3. Cada actor tiene un guion - codigo <br>4. Unos disfraces y sonidos <br>5. paneles de acciones. Mover, control…"
         },
         {
             title: "Evento de Bandera - 2",
-            img: "./gifs/EventoBandera.gif",
+            img: "../gifs/EventoBandera.gif",
             text: "Al pulsar la bandera verde el juego comienza. Para detectar si se presiona la bandera se usa el bloque de ‘al hacer click en’<br><br>Arrastra la el bloque y haz que en comenzar se mueva 10 pasos."
         },
         {
             title: "Panel de movimiento - 3",
-            img: "./gifs/movimiento.gif",
+            img: "../gifs/movimiento.gif",
             text: "En el panel de movimiento podemos encontar los bloques de mover, girar, y apuntar en la direccion. <br><br> Si apuntamos en dirección:<br><br> 90 es derecha <br><br> -90 es izquierda <br><br> 0 es arriba<br><br> 180 es abajo <br><br> <strong>Pruebalo<strong>"
         },
         {
             title: "Coordenadas - 4",
-            img: "./gifs/coordenadas1.gif",
+            img: "../gifs/coordenadas1.gif",
             text: "Cuando movemos un personaje sus coordenadas <strong>x</strong> e <strong>y</strong> cambian su numero.<br>Si arrastramos podemos observar como x e y cambian y determina una posición.<br><br><h3>Prueba a mover el personaje y fijate en sus coordenadas.</h3> "
         },
         {
             title: "Coordenadas - 4.2",
-            img: "./gifs/coordenadas2.gif",
+            img: "../gifs/coordenadas2.gif",
             text: "Si arrastramos el personaje y usamos el bloque 'ir a posición', podemos desplazarlo a donde queramos al inicio del juego."
         },
         {
             title: "Deslizar - 5",
-            img: "./gifs/deslizar1.gif",
+            img: "../gifs/deslizar1.gif",
             text: "Cuando comience el juego: <br>1. Haz que el pesonaje comience de una posición.<br><br>2. Arrastralo a otra posición y fijate en las coordenadas. <br><br>3. haz que se deslice a esa posición<br><br><h3>¿Que ha pasado?<h3>"
         },
         {
             title: "Al hacer... - 6",
-            img: "./gifs/Alhacerclick.gif",
+            img: "../gifs/Alhacerclick.gif",
             text: "Podemos detectar al hacer click en un objeto y tambien al apretar una tecla. <br> <br> <h3>Prueba a hacer que el personaje se pueda mover izquierda y derecha y al hacer click se haga mas grande</h3>"
         },
         {
             title: "Otros objetos- 7",
-            img: "./gifs/otrosObjetos.gif",
+            img: "../gifs/otrosObjetos.gif",
             text: "Otros objetos pueden tener bloques tambien y estos seran diferentes para cada actor.<br><br><h3>Se creativo e inventate algo!</h3>"
         },
         ]
@@ -48,37 +48,37 @@ export const data = [
         name: "prjt2",
         instructions: [{
             title: "Animación - 1",
-            img: "./gifs/esperardisfraz.gif",
+            img: "../gifs/esperardisfraz.gif",
             text: "Haz que el gato parezca que camine: <br><br>Espera, muevete, cambia el vestido varias veces"
         },
         {
             title: "Animación - 2",
-            img: "./gifs/repetir.gif",
+            img: "../gifs/repetir.gif",
             text: "Repíte las instrucciones para que parezca que camine. "
         },
         {
             title: "Rebotar - 3",
-            img: "./gifs/rebotar.gif",
+            img: "../gifs/rebotar.gif",
             text: "El  'Rebota en tocar un borde', sirve para comprobar si se toca un borde, y en el caso de que si, rebote. Si esto lo repetimos todo el tiempo junto a moverse rebotara en chocar con el borde."
         },
         {
             title: "Pequeño proyecto- 4.1",
-            img: "./gifs/esconder.gif",
+            img: "../gifs/esconder.gif",
             text: "Haz que el gato se mueva izquierda y derecha sin parar, y en clicar en el desaparezca. Puedes hacer desaparecer con el bloque 'esconder'"
         },
         {
             title: "Pequeño proyecto- 4.2",
-            img: "./gifs/esconder2.gif",
+            img: "../gifs/esconder2.gif",
             text: "Si le damos a la bandera el gato ya no esta. <br><br> Haz que reaparezca cada vez que le damos a la bandera verde, <br><br> Haz que siempre vaya al medio."
         },
         {
             title: "Pequeño proyecto- 4.3",
-            img: "./gifs/esconder3.gif",
+            img: "../gifs/esconder3.gif",
             text: "Puedes crear un numero aleatorio con el bloque 'numero aleatorio'<br><br> Haz que todo el tiempo gire un numero aleatorio entre -20 y 20. <br><br><h3>El personaje camina!</h3>"
         },
         {
             title: "Pequeño proyecto- 4.4",
-            img: "./gifs/esconder4.gif",
+            img: "../gifs/esconder4.gif",
             text: "Click derecho sobre el personaje => duplicar muchas veces <br><br><h3>Mejora el juego!</h3>"
         }]
     },
@@ -86,32 +86,32 @@ export const data = [
         name: "prjt3",
         instructions: [{
             title: "Juego del Laberino - Colocar y dibujar nivel",
-            img: "./gifs/laberinto1.gif",
+            img: "../gifs/laberinto1.gif",
             text: "Haz pequeño al jugador y dibuja un nivel en el fondo."
         },
         {
             title: "Juego del Laberino - Dibuja una meta",
-            img: "./gifs/laberinto2.gif",
+            img: "../gifs/laberinto2.gif",
             text: "Dibuja un circulo del color que quieras, este será la meta."
         },
         {
             title: "Juego del Laberino - Condiciones",
-            img: "./gifs/laberinto3.gif",
+            img: "../gifs/laberinto3.gif",
             text: "Una condición es una pregunta. Para hacer mover al jugador a la derecha, repetiremos la pregunta 'si el jugador pulso la flecha derecha.<br><br>En el Caso de pulsar la tecla derecha tendrá que apuntar a la derecha y moverse.<br><br><h3>Haz preguntas para combrobar todo el tiempo si pulsa arriba, abajo, derecha e izquierda y acaba de hacer los controles.</h3>'"
         },
         {
             title: "Juego del Laberino - Tocar paredes",
-            img: "./gifs/laberinto4.gif",
+            img: "../gifs/laberinto4.gif",
             text: "Comprobaremos con una pregunta, si el personaje toca el color negro, si lo hace queremos que vuelva a la posición inicial. <br><br><h3>Cuando toquemos la pared vuelve a comenzar</h3>"
         },
         {
             title: "Juego del Laberino - Tocar meta",
-            img: "./gifs/laberinto5.gif",
+            img: "../gifs/laberinto5.gif",
             text: "Cuando toque el color de la meta, debe de superar el nivel y moverse al siguiente fondo.</h3>"
         },
         {
             title: "Juego del Laberino - Enemigo",
-            img: "./gifs/laberinto6.gif",
+            img: "../gifs/laberinto6.gif",
             text: "Crea enemigos que cuando choquen con las paredes reboten aleatoriamente. <br><br><h3>Con una pregunta comprueba si choca con un borde y apunta a una direccion aleatoria</h3>"
         },
         {
@@ -124,32 +124,32 @@ export const data = [
         name: "prjt4",
         instructions: [{
             title: "Juego de esquivar coches - Movimiento",
-            img: "./gifs/coches1.gif",
+            img: "../gifs/coches1.gif",
             text: "El gato debe poder moverse arriba y abajo."
         },
         {
             title: "Juego de esquivar coches - Coche",
-            img: "./gifs/coches2.gif",
+            img: "../gifs/coches2.gif",
             text: "el coche debe aparecer en una posición aleatoria entre arriba y abajo <br><br> <h3>Arrastra el personaje arriba y abajo y fijate en la coordenada y</h3><br><h3>Haz que vaya a la izquierda y en Y aleatoria.</h3>"
         },
         {
             title: "Juego de esquivar coches - Coche",
-            img: "./gifs/coches3.gif",
+            img: "../gifs/coches3.gif",
             text: "EL coche apunta a la izquierda y se mueve todo el rato"
         },
         {
             title: "Juego de esquivar coches - Posición coche a la izquierda del todo",
-            img: "./gifs/coches4.gif",
+            img: "../gifs/coches4.gif",
             text: "Fijate cual es la posición x del coche a la izquierda del todo del coche. <br><br><h3>Arrastra el coche hacia la izquierda, ves como el numero de la x se va haciendo mas pequeño?<br><br><h3>Explicaselo al profe</h3></h3>"
         },
         {
             title: "Juego de esquivar coches - Coche vuelve a la derecha",
-            img: "./gifs/coches5.gif",
+            img: "../gifs/coches5.gif",
             text: "Para comprobar si el coche llego a la izquierda, podemos hacer la pregunta, si la posición x es menor de tal numero. Entonces devolveremos al coche a la derecha de la pantalla, en una posición 'y' aleatoria, al igual que hicimos anteriormente."
         },
         {
             title: "Juego de esquivar coches - Muerte!",
-            img: "./gifs/coches6.gif",
+            img: "../gifs/coches6.gif",
             text: "<h2>Haz que en tocar el coche, el gato muera.</h2> "
         },]
     },
@@ -157,7 +157,7 @@ export const data = [
         name: "prjt5",
         instructions: [{
             title: "Botones",
-            img: "./gifs/mens1.gif",
+            img: "../gifs/mens1.gif",
             text: "Los mensajes sirven para que desde un actor, le digamos a otro actor que puede comenzar a hacer algo.<br><br><h3>Por ejemplo, con un boton, cada vez que cliquemos, que envie un mensaje. El polluelo, cuando reciba el mensaje, que vaya a una posición aleatoria</h3>"
         },
         {
